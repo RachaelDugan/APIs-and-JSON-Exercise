427f18ff7251ec199a2c8dd7b194964847ebe0a2
RachaelDugan <118020235+RachaelDugan@users.noreply.github.com>
2023-01-04 14:21:13 -0700

    initial commit

--- APIsAndJSON/Program.cs
+++ APIsAndJSON/Program.cs
@@ -1,10 +1,25 @@
-﻿namespace APIsAndJSON
+﻿using Newtonsoft.Json.Linq;
+
+namespace APIsAndJSON
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+           var client = new HttpClient();
+           var quote = new QuoteGenClass(client);
+            Console.WriteLine("A Conversation Between Two Very Different People");
+            Console.WriteLine("------------------------------------------------");
+            for (int i = 0; i < 5; i++)
+                {
+                Console.WriteLine();
+                Console.WriteLine($"Ye:    {quote.KanyeQuote()}");
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine($"NAME REDACTED:    {quote.NameRedactedQuote()}");
+                Console.WriteLine();
+                }
+           
         }
     }
 }
---
+++ APIsAndJSON/QuoteGenClass.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APIsAndJSON
+{
+    internal class QuoteGenClass
+    {
+        private HttpClient _client;
+
+        public QuoteGenClass(HttpClient client)
+        {
+            
+            _client = client;
+        }
+        public string KanyeQuote()
+        {
+            var client = new HttpClient();
+
+            var kanyeURL = "https://api.kanye.rest";
+            var kanyeResponse = _client.GetStringAsync(kanyeURL).Result;
+            var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+            return kanyeQuote;
+        }
+        public string NameRedactedQuote()
+        {
+            var client = new HttpClient();
+
+            var ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+            var ronResponse = _client.GetStringAsync(ronURL).Result;
+            var ronQuote = JArray.Parse(ronResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+            return ronQuote;
+        }
+    }
+}
